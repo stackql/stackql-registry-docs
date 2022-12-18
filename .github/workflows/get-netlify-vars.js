c09e636d1ab19133c0650cc36e0741e989ddd88d
Yuncheng <fabioyang781@gmail.com>
2022-12-18 19:09:49 +1100

    test

--- .github/workflows/get-netlify-vars.js
+++ .github/workflows/get-netlify-vars.js
@@ -1,4 +1,8 @@
 module.exports = async ({ github, context, core, pathOutput }) => {
 console.log('context is %o', context)
+const workspace = process.env.GITHUB_WORKSPACE
+const contents = readFileSync(`${workspace}/providers.txt`, 'utf-8');
+console.log('contents are %o', contents)
+const arr = contents.split(/\r?\n/);
 
 }
