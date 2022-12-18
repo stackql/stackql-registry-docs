6eae0df4570854d48608d729ecebd2b6586a7291
Yuncheng <fabioyang781@gmail.com>
2022-12-18 23:07:39 +1100

    test set providers as env var

--- .github/workflows/get-netlify-vars.js
+++ .github/workflows/get-netlify-vars.js
@@ -1,8 +1,3 @@
 module.exports = async ({ github, context, core, pathOutput }) => {
-console.log('context is %o', context)
-const workspace = process.env.GITHUB_WORKSPACE
-const contents = readFileSync(`${workspace}/providers.txt`, 'utf-8');
-console.log('contents are %o', contents)
-const arr = contents.split(/\r?\n/);
 
 }
