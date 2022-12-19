9902955e45f7fe67992ae6b2e7ee0552a6770750
Yuncheng <fabioyang781@gmail.com>
2022-12-19 14:25:37 +1100

    test

---
+++ ci-scripts/get-netlify-vars.js
@@ -0,0 +1,6 @@
+module.exports = async ({ github, context, core, pathOutput }) => {
+//1. set different config to each site as github action env vars
+//2. use provider to set out, so next step will use those Netlify configs
+//3. If it multiple, or updating root, output multiple configs, and loop as matrix in next step
+console.log('in the scripts')
+}
