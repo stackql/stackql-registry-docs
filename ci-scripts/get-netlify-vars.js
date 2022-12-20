29dd3b23b8b1f576e971c7a76812a6c9d1027666
Yuncheng <fabioyang781@gmail.com>
2022-12-20 23:46:25 +1100

    test what is in the event_path env var

--- ci-scripts/get-netlify-vars.js
+++ ci-scripts/get-netlify-vars.js
@@ -5,8 +5,7 @@ module.exports = async ({ github, context, core, pathOutput }) => {
 //1. set different config to each site as github action env vars
 //2. use provider to set out, so next step will use those Netlify configs
 //3. If it multiple, or updating root, output multiple configs, and loop as matrix in next step
-const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
-
-    console.log(event.head_commit.modified);
+    const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
+    console.log(event);
 
 }
