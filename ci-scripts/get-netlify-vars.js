f78fc60c8637e56ab42e8de9e478d50334b7d42c
Yuncheng <fabioyang781@gmail.com>
2022-12-21 00:09:40 +1100

    test if provider is split correctly

--- ci-scripts/get-netlify-vars.js
+++ ci-scripts/get-netlify-vars.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
 
+const regex = /^docs/[a-zA-Z0-9_-]+-docs/providers/[a-zA-Z0-9_-]+$/;
+
+const isMatchRegex = (path) => regex.test(path);
 
 module.exports = async ({ github, context, core, pathOutput }) => {
 //1. set different config to each site as github action env vars
 //2. use provider to set out, so next step will use those Netlify configs
 //3. If it multiple, or updating root, output multiple configs, and loop as matrix in next step
     const diffs = fs.readFileSync('diff.txt', 'utf-8').split('\n').filter(Boolean)
-    const providers = diffs.map(diff => diff.split('/')[2]).filter(Boolean)
+    console.log(diffs)
+    const providers = diffs.map(diff => {
+        if(isMatchRegex(diff)){
+            return diff.split('/')[2].split('-docs')[0]
+        }}).filter(Boolean)
     console.log(providers)
 
 }
