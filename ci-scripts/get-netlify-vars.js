d59ad2f0169099188111243e7c44c504e667f806
Yuncheng <fabioyang781@gmail.com>
2022-12-21 00:24:58 +1100

    test changed files action

--- ci-scripts/get-netlify-vars.js
+++ ci-scripts/get-netlify-vars.js
@@ -12,7 +12,7 @@ module.exports = async ({ github, context, core, pathOutput }) => {
 //1. set different config to each site as github action env vars
 //2. use provider to set out, so next step will use those Netlify configs
 //3. If it multiple, or updating root, output multiple configs, and loop as matrix in next step
-    const changedFiles = core.getInput('changedFiles')
+    const changedFiles = process.env.CHANGED_FILES
     console.log('changedFiles', changedFiles)
     const diffs = fs.readFileSync('diff.txt', 'utf-8').split('\n').filter(Boolean)
     console.log(diffs)
