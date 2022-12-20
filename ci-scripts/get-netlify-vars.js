932a4d663a13844daa4c25b643a04a07ab6856a0
Yuncheng <fabioyang781@gmail.com>
2022-12-20 23:44:35 +1100

    test

--- ci-scripts/get-netlify-vars.js
+++ ci-scripts/get-netlify-vars.js
@@ -6,12 +6,7 @@ module.exports = async ({ github, context, core, pathOutput }) => {
 //2. use provider to set out, so next step will use those Netlify configs
 //3. If it multiple, or updating root, output multiple configs, and loop as matrix in next step
 const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
-console.log(event.pull_request.changed_files);
 
-if (process.env.GITHUB_EVENT_NAME === "push") {
     console.log(event.head_commit.modified);
-  } else if (process.env.GITHUB_EVENT_NAME === "pull_request") {
-    console.log(event.pull_request.changed_files);
-  }
 
 }
