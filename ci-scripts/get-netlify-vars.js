09a6320f033d4fe3cb3e3206d631ef988ec7e142
Yuncheng <fabioyang781@gmail.com>
2022-12-20 23:35:27 +1100

    test

--- ci-scripts/get-netlify-vars.js
+++ ci-scripts/get-netlify-vars.js
@@ -9,6 +9,6 @@ module.exports = async ({ github, context, core, pathOutput }) => {
 //1. set different config to each site as github action env vars
 //2. use provider to set out, so next step will use those Netlify configs
 //3. If it multiple, or updating root, output multiple configs, and loop as matrix in next step
-const diffArr = readTxtToArr('diff.txt');
-console.log(diffArr);
+const data = fs.readFileSync('diff.txt', 'utf8');
+console.log(data);
 }
