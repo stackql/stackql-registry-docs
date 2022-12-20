f816deacfaebd2bfbe4dbbc3d7eba72f731c8bac
Yuncheng <fabioyang781@gmail.com>
2022-12-20 23:49:17 +1100

    test what is in push

--- ci-scripts/get-netlify-vars.js
+++ ci-scripts/get-netlify-vars.js
@@ -6,6 +6,8 @@ module.exports = async ({ github, context, core, pathOutput }) => {
 //2. use provider to set out, so next step will use those Netlify configs
 //3. If it multiple, or updating root, output multiple configs, and loop as matrix in next step
     const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
-    console.log(event);
+    const diff = fs.readFileSync('diff.txt', 'utf-8')
+    // console.log(event);
+    console.log(diff)
 
 }
