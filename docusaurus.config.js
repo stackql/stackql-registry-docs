350bfc68d5e4bdb774963bf59baa663c89cdcedb
jeffreyaven <javen@avensolutions.com>
2023-01-07 09:05:21 +1100

    removed announcement bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
       textColor: '#1A4E82',
       isCloseable: true,
     },
-    announcementBarActive: true,
+    announcementBarActive: false,
     announcementBarLink: "https://github.com/stackql/stackql",    
     // hubspot: {
     //   accountId: '21220110',
