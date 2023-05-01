57ffc921deb6834c83b9707821eaf70f8ce1fa9d
Jeffrey Aven <javen@avensolutions.com>
2023-05-01 14:30:36 +0800

    updated sidebars remove youtube

--- ci-scripts/get-providers-to-deploy.js
+++ ci-scripts/get-providers-to-deploy.js
@@ -49,6 +49,8 @@ module.exports = async ({ github, context, core, pathOutput }) => {
         
         // if(!(diff.startsWith('.github')) || !(diff.startsWith('scripts'))) globalChange = true;
 
+        globalChange = true;
+
     }).filter(Boolean)
 
     const uniqueProviders = [...new Set(providers)]
