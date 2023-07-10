d4a082c60f4c08cba5f7638ca2cd934fb94ac646
Jeffrey Aven <javen@avensolutions.com>
2023-07-11 08:47:34 +1000

    updates to googleadmin setup instructions

--- ci-scripts/get-providers-to-deploy.js
+++ ci-scripts/get-providers-to-deploy.js
@@ -45,7 +45,7 @@ module.exports = async ({ github, context, core, pathOutput }) => {
         
         // if(!(diff.startsWith('.github')) || !(diff.startsWith('scripts'))) globalChange = true;
 
-        globalChange = true;
+        // globalChange = true;
 
     }).filter(Boolean)
 
